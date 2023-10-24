90c5e2b4ac8bde67e497bdabfa34d7e0eae4e482
Flagg217 <flagg217@gmail.com>
2023-10-24 01:54:18 -0700

    added function for license information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+  if (license == 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  }
+  if (license == 'Apache 2.0') {
+    return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  }
+  if (license == 'GPL 3.0') {
+    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
