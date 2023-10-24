5559761ca63e091aeb9c1c270e06d6985211392c
Flagg217 <flagg217@gmail.com>
2023-10-24 02:03:59 -0700

    added function to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -55,6 +55,40 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+# Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Test](#test)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contribution
+${data.contribution}
+
+## Test
+${data.test}
+
+## License
+${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+
+## Questions
+If you have any questions, please contact me at ${data.email}. You can also find more of my work at [${data.github}]()
+
 
 `;
 }
