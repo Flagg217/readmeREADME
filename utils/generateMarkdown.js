48947def51372b5af6a31235831a5ad6e7a31711
Flagg217 <flagg217@gmail.com>
2023-10-24 01:56:11 -0700

    added function that returns the license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,7 +18,21 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
+function renderLicenseLink(license) {
+  if (license == 'MIT') {
+    return `https://opensource.org/licenses/MIT`;
+  }
+  if (license == 'Apache 2.0') {
+    return `https://opensource.org/licenses/Apache-2.0`;
+  }
+  if (license == 'GPL 3.0') {
+    return `https://www.gnu.org/licenses/gpl-3.0`;
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
